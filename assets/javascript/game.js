ad640c808a0edd2e93ad99e61702bd15115d5764
romibabe <romilla@gmail.com>
2017-06-02 20:03:48 -0400

    logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var questions = [{
+    question: "What was the first animated film released by Walt Disney?",
+    choices: ["Cindrella", "Snow White and the Seven Dwarfs ", "Fantasia", "Alice in Wonderland"],
+    correctAnswer: 2
+}, {
+    question: "In the movie the Lion King who is Simba's uncle?",
+    choices: ["Scar", "Timon", "Mufasa", "Rafiki"],
+    correctAnswer: 1
+}, {
+    question: "What time did Cindrella's godmother say the spell would break as Cindrella was going to the ball?",
+    choices: ["Before the sun goes down", "As soon as morning came", "Midnight", "noon"],
+    correctAnswer: 3
+},  {
+    question: "How many sisters does Ariel have?",
+    choices: ["Six", "No sisters-she's an only merchild", "Eight", "Seven"],
+    correctAnswer: 1
+}, {
+    question: "Which Disney character fell down a rabbit hole?",
+    choices: ["Peter Pan", "Alice", "Aladdin", "Anna"],
+    correctAnswer: 2
+}]; 
+
+
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+
